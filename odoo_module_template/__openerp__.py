aab2ab632d8a7cb17a67be292d6e673f6ef919cf
bashir-alsuty <beshasuty125@hotmail.com>
2016-12-02 09:38:33 -0800

    Update __openerp__.py

--- odoo_module_template/__openerp__.py
+++ odoo_module_template/__openerp__.py
@@ -1,1 +1,36 @@
+# -*- coding: utf-8 -*-
 
+#    Bashir Idirs (Alsuty)
+#    Copyright (C) 2016.
+#
+#    This Code: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+{
+    'name': 'Odoo Module Template',
+    'author': 'Bashir Idris (Alsuty)',
+    'website': 'https://github.com/kodat',
+    'summary': 'It is sample module',
+    'description': """
+            This is template module which contains Form, Tree and kanban view,
+            and one python file contain logic data.
+    """,
+
+    'data': [
+        'view/view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+
+}
